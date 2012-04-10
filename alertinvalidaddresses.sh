71b1df0c329f451021812d7d6aa3098d1f22423e
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-04-10 19:22:55 +0900

    Update maxVersion

--- alertinvalidaddresses.sh
+++ alertinvalidaddresses.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+gi#!/bin/sh
 
 appname=${0##*/}
 appname=${appname%.sh}
 
-cp buildscript/make_new.sh ./
-./make_new.sh $appname version=1
-rm make_new.sh
+cp buildscript/makexpi.sh ./
+./makexpi.sh -n $appname -v
+rm makexpi.sh
